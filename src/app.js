92db3fc24f7a58d786f4317c001c58a66c6f47de
dinomaze <dino@maze.design>
2022-08-18 14:11:42 +0200

    chore: cleanup

--- src/app.js
+++ src/app.js
@@ -4,20 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var isProduction = process.env.NODE_ENV === 'production';
+
+console.log('PRODUCTION', isProduction);
+console.log('PORT', process.env.PORT);
+console.log('PUBLIC PATH', isProduction ? path.join(__dirname, 'public') : path.join(__dirname, '..','public'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(isProduction ? path.join(__dirname, 'public') : path.join(__dirname, '..','public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,7 +31,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ err });
 });
 
 module.exports = app;
